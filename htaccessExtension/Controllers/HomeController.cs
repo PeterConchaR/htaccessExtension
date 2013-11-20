5378ab6c6d26023e2d457f99d968c5827c6f6ce5
Cory Fowler <syntaxc4@gmail.com>
2013-11-19 22:06:43 -0800

    ConversionManager complete. Adding UI to test

--- htaccessExtension/Controllers/HomeController.cs
+++ htaccessExtension/Controllers/HomeController.cs
@@ -16,24 +16,6 @@ namespace htaccessExtension.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine("RewriteEngine On");
-            builder.AppendLine("RewriteBase /myapp/");
-            builder.AppendLine(@"RewriteRule ^index\.html$  welcome.html");
-
-            using (XmlWriter writer = XmlWriter.Create(@"C:\temp\web.config"))
-            {
-                Assembly iisRewriteClient = Assembly.Load("Microsoft.Web.Management.Rewrite.Client, Version=7.2.2.1, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-
-                Type translator = iisRewriteClient.GetType("Microsoft.Web.Management.Iis.Rewrite.Translation.Translator");
-                MethodInfo translate = translator.GetMethod("Translate");
-                var result = translate.Invoke(null, new object[] { builder.ToString(), true, 0 });
-
-                Type rewriteEntry = iisRewriteClient.GetType("Microsoft.Web.Management.Iis.Rewrite.Translation.RewriteEntry");
-                MethodInfo writeTo = rewriteEntry.GetMethod("WriteTo");
-                writeTo.Invoke(result, new object[] { writer });
-            }
-
             return View();
         }
 	}
--- htaccessExtension/Models/ConversionManager.cs
+++ htaccessExtension/Models/ConversionManager.cs
@@ -1,27 +1,109 @@
-﻿
-namespace htaccessExtension.Models
+﻿namespace htaccessExtension.Models
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using System.Reflection;
-    using System.Text;
-    using System.Web;
+    using System.Xml;
+    using System.Xml.Linq;
 
     public class ConversionManager
     {
+        protected const string _defaultWebConfig = "<configuration><system.webServer></system.webServer></configuration>";
+        protected Action<XmlWriter, object> WriteTo;
+        protected Func<string, bool, int, object> Translate;
+        
         public ConversionManager()
         {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine("RewriteEngine On");
-            builder.AppendLine("RewriteBase /myapp/");
-            builder.AppendLine(@"RewriteRule ^index\.html$  welcome.html");
-
-            Assembly iisRewriteClient = Assembly.Load("Microsoft.Web.Management.Rewrite.Client, Version=7.2.2.1, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-            Type type = iisRewriteClient.GetType("Microsoft.Web.Management.Iis.Rewrite.Translation.Translator");
-
-            MethodInfo translate = type.GetMethod("Translate");
-            translate.Invoke(null, new object[] {builder.ToString(), false, 0});
+            var iisRewriteClient = Assembly.Load("Microsoft.Web.Management.Rewrite.Client, Version=7.2.2.1, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+            CreateTranslateDelegate(iisRewriteClient);
+            CreateRewriteDelegate(iisRewriteClient);
         }
 
+        public string GenerateOrUpdateWebConfig(Stream webconfig, Stream htaccess)
+        {
+            string config = ReadFullFile(webconfig);
+            string apache = ReadFullFile(htaccess);
 
+            return GenerateOrUpdateWebConfig(config, apache);
+        }
+
+        public string GenerateOrUpdateWebConfig(string config, string htaccessFileContent)
+        {
+            bool addWebServerSection = false;
+            if (String.IsNullOrWhiteSpace(config))
+            {
+                config = _defaultWebConfig;
+            }
+
+            var doc = XDocument.Parse(config);
+
+            var webServerSection = FindSingle(doc, "system.webServer");
+            if(webServerSection == default(XElement))
+            {
+                webServerSection = new XElement("system.webServer");
+                addWebServerSection = true;
+            }
+
+            var rewriteSection = FindSingle(webServerSection, "rewrite");
+            if (rewriteSection != default(XElement))
+            {
+                rewriteSection.Remove();
+            }
+
+            webServerSection.Add(GenerateRewriteSectionFromHTAccess(htaccessFileContent));
+
+            if(addWebServerSection)
+            {
+                doc.Root.Add(webServerSection);
+            }
+
+            return doc.ToString();
+        }
+
+        private string GenerateRewriteSectionFromHTAccess(string fileContents)
+        {
+            string config = string.Empty;
+            var result = Translate(fileContents, true, 0);
+
+            using (var xmlString = new StringWriter())
+            {
+                using (var xw = XmlWriter.Create(xmlString))
+                {
+                    WriteTo(xw, result);
+                }
+                config = xmlString.ToString();
+            }
+            return config;
+        }
+
+        private string ReadFullFile(Stream file)
+        {
+            string contents = String.Empty;
+            using (StreamReader sr = new StreamReader(file))
+            {
+                contents = sr.ReadToEnd();
+            }
+            return contents;
+        }
+
+        private static XElement FindSingle(XContainer container, string elementName)
+        {
+            return container.Descendants().SingleOrDefault(e => e.Name.LocalName == elementName);
+        }
+
+        private void CreateTranslateDelegate(Assembly client)
+        {
+            Type translator = client.GetType("Microsoft.Web.Management.Iis.Rewrite.Translation.Translator");
+            MethodInfo translate = translator.GetMethod("Translate");
+            Translate = (Func<string, bool, int, object>)Delegate.CreateDelegate(typeof(Func<string, bool, int>), translate);
+        }
+
+        private void CreateRewriteDelegate(Assembly client)
+        {
+            Type rewriteEntry = client.GetType("Microsoft.Web.Management.Iis.Rewrite.Translation.RewriteEntry");
+            MethodInfo writeTo = rewriteEntry.GetMethod("WriteTo");
+            WriteTo = (Action<XmlWriter, object>)Delegate.CreateDelegate(typeof(Action<XmlWriter, object>), writeTo);
+        }
     }
 }
